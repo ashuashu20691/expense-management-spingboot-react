a39cc8bff0b994c67c8dba8af92a45f9425c7f42
Ashu Kumar <ashuashu20691@gmail.com>
2024-04-04 16:22:48 +0530

    scripts/clone-db.sh

--- scripts/clone-db.sh
+++ scripts/clone-db.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
 # Configuration Variables
 SOURCE_DB_OCID="ocid1.autonomousdatabase.oc1.ap-mumbai-1.anrg6ljrobogfhqajjp7nk74dr3bputn3aa646uistr5xiyxwu5tkh3octuq"
@@ -8,24 +8,23 @@ CPU_CORE_COUNT=3 # Adjust as needed
 DATA_STORAGE_SIZE_IN_TB=1 # Adjust as needed
 COMPARTMENT_OCID="ocid1.compartment.oc1..aaaaaaaavkerdgzd55avjsnomocb2ttxb4j4j7gecxwdfwtstwb2iyowtfba"
 
-# Clone the Autonomous Database
-echo "Cloning the Autonomous Database..."
-CLONE_RESPONSE=$(oci db autonomous-database clone --source-id $SOURCE_DB_OCID \
-                --clone-type FULL \
+# Create Autonomous Database from Clone
+echo "Creating Autonomous Database from Clone..."
+CREATE_RESPONSE=$(oci db autonomous-database create-from-clone \
+                --source-autonomous-database-id $SOURCE_DB_OCID \
+                --display-name "$CLONE_DB_DISPLAY_NAME" \
                 --cpu-core-count $CPU_CORE_COUNT \
                 --data-storage-size-in-tbs $DATA_STORAGE_SIZE_IN_TB \
-                --db-name $CLONE_DB_NAME \
-                --display-name $CLONE_DB_DISPLAY_NAME \
                 --compartment-id $COMPARTMENT_OCID \
                 --wait-for-state AVAILABLE \
                 --query 'data.id' --raw-output)
 
-if [ -z "$CLONE_RESPONSE" ]; then
-    echo "Failed to clone the Autonomous Database."
+if [ -z "$CREATE_RESPONSE" ]; then
+    echo "Failed to create the Autonomous Database from Clone."
     exit 1
 else
-    CLONED_DB_OCID=$CLONE_RESPONSE
-    echo "Successfully cloned the Autonomous Database. OCID: $CLONED_DB_OCID"
+    CLONED_DB_OCID=$CREATE_RESPONSE
+    echo "Successfully created the Autonomous Database from Clone. OCID: $CLONED_DB_OCID"
 fi
 
 # Optionally, Resize the Cloned Database (Upgrade or Downgrade)
